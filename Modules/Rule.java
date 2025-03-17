a35fce96dfeffd049bf6d0986acc6f60bd29b92c
polyqubit <polyumbrial@gmail.com>
2025-03-17 10:20:31 -0700

    unknown errors

--- Modules/Rule.java
+++ Modules/Rule.java
@@ -26,16 +26,34 @@ public class Rule {
 
     // if this does not work, consider Object.equals recursive approach
     public void apply(MathObject m) {
-        if (traverse(m, search)) {
+        if (recursiveapply(m)) {
             System.out.print("Starting rule subtraverse for "); MathPrinter.print(search); System.out.println();
             substitute(m);
         }
     }
 
+    // need an outer traverse layer
+    private boolean recursiveapply(MathObject m) {
+        if(m.type().equals("Number") || m.type().equals("Variable")) {
+            System.out.println("reached end of target");
+            return false;
+        } 
+        if(traverse(m, search)) {
+            return true;
+        }
+        if(m.type().equals("Operator")) {
+            return recursiveapply(((Operator)m).parameter1()) || recursiveapply(((Operator)m).parameter2());
+        }
+        if(m.type().equals("Expression")) {
+            return recursiveapply(((Expression)m).getexpr());
+        }
+        return recursiveapply(m);
+    }
+
     // current target layer, current search(as in this.search) layer
     private boolean traverse(MathObject cur, MathObject s) {
         if (s.type().equals("Any")) {
-            System.out.println("success - found wildcard");
+            System.out.println("success - found wildcard: " + cur.name());
             mathmap.put(s.name(), cur);
             return true;
         }
@@ -94,18 +112,36 @@ public class Rule {
             return traverse(ecur.getexpr(), es.getexpr());
         }
         // TODO: need case for function
+        System.out.println("failure - no cases matched");
         return false;
+    }
+
+    private int count(MathObject m) {
+        if(m.type().equals("Number")||m.type().equals("Variable")) {
+            return 1;
+        }
+        if(m.type().equals("Operator")) {
+            Operator o = (Operator)m;
+            return count(o.parameter1()) + count(o.parameter2()) + 1;
+        }
+        if(m.type().equals("Expression")) {
+            Expression e = (Expression)m;
+            return count(e.getexpr()); // no need to count expr
+        }
+        return 0;
     }
 
     // substitute traverse
     // current target layer, current replacement layer
     private void subtraverse(MathObject cur, MathObject r) {
         if (r.name().equals("Number")) {
+            System.out.println(" - setting num " + ((Num)cur).value());
             ((Num)cur).setval(tempnums.poll().value());
             return;
         }
 
         if (r.name().equals("Variable")) {
+            System.out.println(" - setting var " + cur.name());
             ((Variable)cur).settoken(varmap.get(((Variable)r).token()));
             return;
         }
@@ -119,6 +155,7 @@ public class Rule {
             Expression ecur = (Expression) cur;
             Expression er = (Expression) r;
             if(er.getexpr().type().equals("Any")) {
+                System.out.print(" - setting expr "); MathPrinter.print(mathmap.get(er.getexpr().name())); System.out.println();
                 ecur.setexpr(mathmap.get(er.getexpr().name()));
                 // System.out.println(mathmap.get(er.name()).name());
                 return;
@@ -129,20 +166,37 @@ public class Rule {
         if (cur.type().equals("Operator")) {
             Operator ocur = (Operator) cur;
             Operator or = (Operator) r;
-            boolean skip = false;
+            boolean skip1 = false;
+            boolean skip2 = false;
             if(or.parameter1().type().equals("Any")) {
+                System.out.print(" - setting expr "); MathPrinter.print(mathmap.get(or.parameter1().name())); System.out.println();
                 ocur.setP1(mathmap.get(or.parameter1().name()));
-                skip = true;
+                skip1 = true;
             }
             if(or.parameter2().type().equals("Any")) {
                 ocur.setP2(mathmap.get(or.parameter2().name()));
-                skip = true;
+                System.out.print(" - setting expr "); MathPrinter.print(mathmap.get(or.parameter2().name())); System.out.println();
+                skip2 = true;
             }
-            if(skip) {
-                return;
-            }
-            subtraverse(ocur.parameter1(), or.parameter1());
-            subtraverse(ocur.parameter2(), or.parameter2());
+            // if(skip) {
+            //     System.out.println(" - completed operator, returning");
+            //     return;
+            // }
+            if(!skip1){subtraverse(ocur.parameter1(), or.parameter1());}
+            if(!skip2){subtraverse(ocur.parameter2(), or.parameter2());}
+        }
+    }
+
+    private void subrecursiveapply(MathObject m) {
+        if(traverse(m, search)) {
+            subtraverse(m, replace);
+        }
+        if(m.type().equals("Operator")) {
+            subrecursiveapply(((Operator)m).parameter1());
+            subrecursiveapply(((Operator)m).parameter2());
+        }
+        if(m.type().equals("Expression")) {
+            subrecursiveapply(((Expression)m).getexpr());
         }
     }
 
@@ -150,6 +204,6 @@ public class Rule {
     // _N(0) + _N(1) = _N(_N(1) + _N(2)) ?
     // _L(0) + _N(0) =
     private void substitute(MathObject m) {
-        subtraverse(m, replace);
+        subrecursiveapply(m);
     }
 }
